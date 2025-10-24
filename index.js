bfd33fcba3fc47c7b3c614f37eaa3301e2721d9e
vigneshckdev <vigneshckdev@gmail.com>
2025-10-24 11:06:05 +0530

    changes in the index.js file

--- index.js
+++ index.js
@@ -142,3 +142,5 @@ app.get('/users', async (req, res) => {
 // ----------------- Start Server -----------------
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
