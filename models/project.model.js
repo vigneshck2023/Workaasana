3c84763851030268d459ebd151b1f5ff57513492
vigneshckdev <vigneshckdev@gmail.com>
2025-10-29 01:55:53 +0530

    changes in the schema

--- models/project.model.js
+++ models/project.model.js
@@ -16,10 +16,10 @@ const teamSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   members: [
     {
-      name: { type: String, required: true }
-    }
+      name: { type: String, required: true },
+    },
   ],
-  description: String
+  description: String,
 });
 
 
